d6885b477b2ebdeafef020fc9f0c2add4fbc083a
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-05-05 10:37:47 -0600

    Commit 3

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 // import using inquier - set var name = require("package name") 
 // .prompt .then .catch(errors)
 // var name = object then export (call by .x)
-// questions are passed to prompt func as array prompt(array).then(anwers => {
-//     answers.titleName 
-// });
-// const userInput = [title]
-// create obj for each question & for each have name of question & value from user input
-// pass userInput[] into write to file as data parameter
-// should always be standarized
-// add another attributs to each obj in userInput[] 
-// hold [objects of variations]
-// each obj would have a name of variation & true/false attribute 
-// create a badge = https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
 
 const generateMardown = require("./utils/generateMarkdown");
 
 
 //  * ask a bunch of questions using inquirer.prompt() passing questions in as an array
+// questions are passed to prompt func as array 
+// create obj for each question & for each have name of question & value from user input
+// hold [objects of variations]
+// each obj would have a name of variation & true/false attribute 
 const questions = [
-
+    inquirer.prompt()
+    prompt(array).then(anwers => {
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+        answers.titleName
+    });
 ];
+
+// const userInput = [title]
+// pass userInput[] into write to file as data parameter
+// add another attributs to each obj in userInput[] 
+const userInput = [title]
+const userInput = [description]
+const userInput = [tableOfContents]
+const userInput = [installation]
+const userInput = [usage]
+const userInput = [license]
+const userInput = [contributing]
+const userInput = [tests]
+const userInput = [questions]
+const userInput = [githubProfilePicture]
+const userInput = [githubEmail]
 
 // * merge inquirer answers with github data
 
@@ -31,7 +51,7 @@ axios({
     data: {
 
     }
-  });
+});
 
 
 // * call generateMarkdown with combined data  - inside this function, format your document with dynamic data
@@ -53,3 +73,7 @@ init(data.parse);
 // fs to write file (fs.writefile) - create md file 
 // use escape charachters to format
 // access md file & change it
+fs.writeToFile
+
+
+// create a badge = https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
